073fd3042c2a93a7e234648ad966ba1b5f83ce6b
David <exegetor@gmail.com>
2023-09-02 23:05:11 -0400

    add PUTS routine with startup/halt message

--- src/main.asm
+++ src/main.asm
@@ -12,13 +12,55 @@ org 7C00h
 ; We must tell the assembler to generate instructions of that type.
 bits 16
 
+; define macro for CR/LF
+%define ENDL 0Dh, 0Ah
 
 ;-------------------------------------------------------------------------------
 ; BEGIN CODE
 ;
 
 ;-------------------------------------------------------------------------------
+start:
+    jmp main
+
+;-------------------------------------------------------------------------------
+;   SUBROUTINE PUTS: put string to screen
+;   PARAMETERS:
+;       ds:si points to string
+puts:
+    push si
+    push ax
+.loop:
+    lodsb       ; al = next character
+    or al, al   ; don't change al, but set zero flag if al is 0
+    jz .done
+    mov ah, 0Eh ; print in TTY mode
+    mov bh, 0   ; page number 0.  (bl not used in text mode)
+    int 10h     ; call BIOS putchar subroutine
+    jmp .loop
+.done:
+    pop ax
+    pop si
+    ret
+
+
+;-------------------------------------------------------------------------------
 main:
+    ; initialize segment registers
+    mov ax, 0   ; (because we can't write directly to segment registers)
+    mov ds, ax
+    mov es, ax
+    ; initialize stack address
+    mov ss, ax
+    mov sp, 7c00h
+
+    ; display startup message
+    mov si, msg_startup
+    call puts
+
+    ; display halting message
+    mov si, msg_halt
+    call puts
 .halt:
     cli         ; prevents an interrupt from allowing us to escape from the 'hlt' instruction
     hlt         ; halts the processor
@@ -28,6 +70,10 @@ main:
 ;-------------------------------------------------------------------------------
 ; DATA SECTION
 ;
+
+msg_startup: db 'starting BMOS...', ENDL, 0
+msg_halt:    db 'halting processor!', ENDL, 0
+
 ; This file is the Boot Sector of our Boot Device (an emulated 3.5"
 ; 1.44MB floppy disk).  As such, it must assemble into exactly 512
 ; bytes.  We should zero-pad the rest this memory block, and add the
